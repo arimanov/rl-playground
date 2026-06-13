32fecb6c251b509bbe5ed8ee9035076ccc103d87
Maksim Tsipun <solcreative@gmail.com>
2026-06-13 18:45:28 -0400

    feat: collision

--- main.c
+++ main.c
@@ -6,25 +6,24 @@ int main(void) {
     SetWindowState(FLAG_WINDOW_RESIZABLE);
     SetTargetFPS(60);
 
-    float x = 400;
-    float y = 225;
-    float speed = 200.0f;
+    Rectangle player = { 400, 225, 50, 50 };
+    Rectangle obstacle = { 300, 200, 80, 80 };
 
     while (!WindowShouldClose()) {
-        // --- UPDATE ---
         float dt = GetFrameTime();
-        //TraceLog(LOG_INFO, "x = %f", dt);
 
-        if (IsKeyDown(KEY_RIGHT)) x += speed * dt;
-        if (IsKeyDown(KEY_LEFT))  x -= speed * dt;
-        if (IsKeyDown(KEY_DOWN))  y += speed * dt;
-        if (IsKeyDown(KEY_UP))    y -= speed * dt;
+        if (IsKeyDown(KEY_RIGHT)) player.x += 200.0f * dt;
+        if (IsKeyDown(KEY_LEFT))  player.x -= 200.0f * dt;
+        if (IsKeyDown(KEY_DOWN))  player.y += 200.0f * dt;
+        if (IsKeyDown(KEY_UP))    player.y -= 200.0f * dt;
 
-        // --- DRAW ---
+        bool hit = CheckCollisionRecs(player, obstacle);
+
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawRectangle(x - 25, y - 25, 50, 50, MAROON);
-            DrawFPS(10, 10);
+            DrawRectangleRec(obstacle, DARKGRAY);
+            DrawRectangleRec(player, hit ? RED : MAROON);
+            DrawText(hit ? "HIT!" : "no collision", 10, 10, 20, BLACK);
         EndDrawing();
     }
 
